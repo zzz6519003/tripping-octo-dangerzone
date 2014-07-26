6be34ac74e96d0e070a0a179d30c7020e1f03bb1
zzz6519003 <zhengzhongzhao@gmail.com>
2014-07-26 17:20:40 +0800

    implement basic authentication

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ISee::Application.config.secret_key_base = '704761ff7196c228b0afd6722737a1b05c91718df4a895c04fed3f5be2467aabe156e6c1cc4a203bf046bee6895394a5f1dbc203acaa49689066d143cfde5d6c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ISee::Application.config.session_store :cookie_store, key: '_iSee_session'
